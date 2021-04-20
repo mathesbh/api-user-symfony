315426f42f7a40b34dd48a2e207b840713c78fa7
mathesbh <mathesbh@gmail.com>
2021-04-20 20:48:41 -0300

    Cria controller createUser

---
+++ src/Controller/CreateUserAction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CreateUserAction
+{
+  public function __construct(private EntityManagerInterface $entityManager)
+  {
+  }
+
+  #[Route("/users", methods:["POST"])]
+  public function __invoke(Request $request): Response
+  {
+    $requestContent = json_decode($request->getContent(), true);
+
+    $user = new User();
+    $user->setFirstName($requestContent['firstName']);
+    $user->setLastName($requestContent['lastName']);
+
+    $this->entityManager->persist($user);
+    $this->entityManager->flush();
+
+    return new JsonResponse([
+      'status' => 'ok'
+    ], Response::HTTP_CREATED, [
+      'Location' => '/users/' . $user->getId()
+    ]);
+  }
+}
