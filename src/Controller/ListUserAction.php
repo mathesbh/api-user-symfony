d2fe5220d841cbb67052984634aeff03e256df54
mathesbh <mathesbh@gmail.com>
2021-04-20 21:35:19 -0300

    Cria controller listagem de usuarios

---
+++ src/Controller/ListUserAction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListUserAction
+{
+  public function __construct(private EntityManagerInterface $entityManager)
+  {
+    
+  }
+
+  #[Route("/users", methods:["GET"])]
+  public function __invoke(): Response
+  {
+    $repository = $this->entityManager->getRepository(User::class);
+    $users = $repository->findAll();
+
+    $response = [];
+
+    foreach($users as $user){
+      $response[] = [
+        'id' => $user->getId(),
+        'firstName' => $user->getFirstName(),
+        'lastName' => $user->getLastName(),
+        'createdAt' => $user->getCreatedAt()->format('Y-m-d H:i:s'),
+      ];
+    }
+
+    return new JsonResponse($response, Response::HTTP_ACCEPTED);
+  }
+}
