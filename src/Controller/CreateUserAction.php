293f61ddff2574be4bb1ee5d076731c0406c57dd
mathesbh <mathesbh@gmail.com>
2021-04-25 09:42:04 -0300

    Valida criação de usuário

--- src/Controller/CreateUserAction.php
+++ src/Controller/CreateUserAction.php
@@ -9,10 +9,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CreateUserAction
 {
-  public function __construct(private EntityManagerInterface $entityManager, private SerializerInterface $serializer)
+  public function __construct(private EntityManagerInterface $entityManager, private SerializerInterface $serializer, private ValidatorInterface $validator)
   {
   }
 
@@ -20,6 +22,24 @@ class CreateUserAction
   public function __invoke(Request $request): Response
   {
     $user = $this->serializer->deserialize($request->getContent(), User::class, format: 'json');
+
+    $errors = $this->validator->validate($user);
+
+    if(count($errors) > 0){
+      $violations = array_map(function(ConstraintViolationInterface $violation){
+        return [
+          'path' => $violation->getPropertyPath(),
+          'message' => $violation->getMessage(),
+        ];
+      }, iterator_to_array($errors));
+
+      $response = [
+        'error' => 'As informações enviadas estão incorretas',
+        'violations' => $violations,
+      ];
+
+      return new JsonResponse($response, Response::HTTP_BAD_REQUEST);
+    }
 
     $this->entityManager->persist($user);
     $this->entityManager->flush();
