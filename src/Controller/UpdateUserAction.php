03746e19679c6f22fdbeae5166543c9decc0d1bb
mathesbh <mathesbh@gmail.com>
2021-04-26 13:13:27 -0300

    Valida atualização de usuário

--- src/Controller/UpdateUserAction.php
+++ src/Controller/UpdateUserAction.php
@@ -7,25 +7,53 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UpdateUserAction
 {
 
-  public function __construct(private EntityManagerInterface $entityManager)
+  public function __construct(private EntityManagerInterface $entityManager, private ValidatorInterface $validator, private SerializerInterface $serializer)
   {
   }
 
   #[Route('/users/{id}', methods: ['PUT'])]
   public function __invoke(int $id, Request $request) : Response
   { 
-    $requestContent = json_decode($request->getContent(), true);
 
-    $repository = $this->entityManager->getRepository(User::class);
-    $user = $repository->find($id);
+    $request = $this->serializer->deserialize($request->getContent(), User::class,  format: 'json');
 
-    $user->setFirstName($requestContent['firstName']);
-    $user->setLastName($requestContent['lastName']);
-    $user->setEmail($requestContent['email']);
+    $user = $this->entityManager->find(User::class, $id);
+
+    if(null === $user){
+      return new JsonResponse([
+        'error' => 'User not found'
+      ], Response::HTTP_NOT_FOUND);
+    }
+
+    $errors = $this->validator->validate($request);
+
+    if(count($errors) > 0){
+      $violations = array_map(function(ConstraintViolationInterface $violation){
+        return [
+          'path' => $violation->getPropertyPath(),
+          'message' => $violation->getMessage(),
+        ];
+      }, iterator_to_array($errors));
+
+      $response = [
+        'error' => 'As informações enviadas estão incorretas',
+        'violations' => $violations,
+      ];
+
+      return new JsonResponse($response, Response::HTTP_BAD_REQUEST);
+    }
+
+    $user->setFirstName($request->getFirstName());
+    $user->setLastName($request->getLastName());
+    $user->setEmail($request->getEmail());
 
     $this->entityManager->persist($user);
     $this->entityManager->flush();
