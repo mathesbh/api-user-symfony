fd6ef0a94f1b4f65d8577497f943993aaee7ca4a
mathesbh <mathesbh@gmail.com>
2021-04-26 14:17:26 -0300

    Adiciona teste funcional criação de usuário

---
+++ tests/Controller/CreateUserActionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Test\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class CreateUserActionTest extends WebTestCase
+{
+  public function test_create_user(): void
+  {
+    $client = static::createClient();
+
+    $client->request(method: 'POST', uri: '/users',
+      content: json_encode([
+        'firstName' => 'Teste',
+        'lastName' => 'Funcional',
+        'email' => 'email@teste.funcional',
+        'address' => [
+          'state' => 'MG',
+          'city' => 'BH',
+          'district' => 'Centro',
+          'street' => 'Av do Contorno',
+          'number' => 10,
+          'complement' => 'teste'
+        ]
+      ])
+    );
+
+    $statusCode = $client->getResponse()->getStatusCode();
+    $this->assertSame(Response::HTTP_CREATED, $statusCode);
+  }
+
+  public function test_create_user_with_invalid_data(): void
+  {
+    $client = static::createClient();
+
+    $client->request(method: 'POST', uri:'/users',
+      content: json_encode([
+        'firstName' => 'Teste',
+        'email' => 'email@teste.funcional',
+      ])
+    );
+
+    $statusCode = $client->getResponse()->getStatusCode();
+    $this->assertSame(Response::HTTP_BAD_REQUEST, $statusCode);
+  }
+}
